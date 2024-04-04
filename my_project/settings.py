7ab11e60f95284f3963068819e45185b02be7336
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-04 15:28:08 +0000

    setting - fix error by adding loclhost to allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kkn_%w6g+jj_v4tzgs#ndpsq30&)$sdvxkl-hllheg8lqc=z(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
