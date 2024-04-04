dfdce0fee0f462a2e223028dc71625b389c73691
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-04 17:28:31 +0000

    urls - add urlpatterns path about

--- about/views.py
+++ about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This Is The About Page")
+    return HttpResponse("This would be the about page")
