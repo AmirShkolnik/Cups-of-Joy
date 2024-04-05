bc98868f837e63e3ea252e7890f08b449142c1b9
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-05 13:43:49 +0000

    setting.py - remove komma after herokuapp.com

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com',
                 '127.0.0.1', 'localhost',
-                '.herokuapp.com',]
+                '.herokuapp.com']
 
 
 # Application definition
