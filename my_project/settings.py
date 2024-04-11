d2ce0d6bf3dba30bc58a36b7ca5d4af0ef729125
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-11 14:04:36 +0000

    blog - urls - PostList class-based view named home

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kkn_%w6g+jj_v4tzgs#ndpsq30&)$sdvxkl-hllheg8lqc=z(x
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com', '127.0.0.1', 'localhost', '.herokuapp.com']
 
