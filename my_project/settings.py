93e82fef95865743a1a17f690ebd97af4fb53eb8
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-13 17:43:17 +0000

    Add a new path for post_detail in the blog/urls.py file.

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kkn_%w6g+jj_v4tzgs#ndpsq30&)$sdvxkl-hllheg8lqc=z(x
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '8000-amirshkolnik-jobfinder-0mqhu2bjjsz.ws-eu110.gitpod.io']
 
