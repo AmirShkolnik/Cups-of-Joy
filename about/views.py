218b57fb59044a160be3b7e33d17d958f25610ad
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-16 14:08:13 +0000

    about/models.py - add class CollaborateRequest

--- about/views.py
+++ about/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
 
-# Create your views here.
-
 
 def about_me(request):
     """
@@ -11,4 +9,12 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
-    return render(request, "about/about.html", {"about": about, "collaborate_form": collaborate_form},)
+
+    return render(
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
+    )
