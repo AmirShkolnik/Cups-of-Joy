4df612a6d3b9bd0f0d55df1111d7c505421171f0
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-18 19:23:57 +0000

    my_project/settings - fix pep8ci errors lines too long

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,13 +25,15 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kkn_%w6g+jj_v4tzgs#ndpsq30&)$sdvxkl-hllheg8lqc=z(x'
+SECRET_KEY = 'django-insecure-kkn_%w6g+jj_v4tzgs#ndpsq'
+'30&)$sdvxkl-hllheg8lqc=z(x'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '8000-amirshkolnik-jobfinder-0mqhu2bjjsz.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com',
+                 '8000-amirshkolnik-jobfinder-0mqhu2bjjsz.ws-eu110.gitpod.io']
 
 
 # Application definition
@@ -121,16 +123,20 @@ CSRF_TRUSTED_ORIGINS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
