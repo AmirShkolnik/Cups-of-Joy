b885bede20e49acc6d63e3790b8def71147ab11e
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-19 12:07:03 +0000

    about/test_form - add name test

---
+++ about/test_forms.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .forms import CollaborateForm
+
+
+class TestCollaborateForm(TestCase):
+
+    def test_form_is_valid(self):
+        """ Test for all fields"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Name was not provided, but the form is valid"
+        )
