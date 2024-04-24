9edc7a483824eebfc251f0f839333419945d02e4
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-24 21:18:42 +0000

    choosemug/admin: create list display - name and price

--- choosemug/admin.py
+++ choosemug/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Mom
 # Register your models here.
+
+class MomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'priceM', 'priceL')
+
+admin.site.register(Mom, MomAdmin)
--- choosemug/models.py
+++ choosemug/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class mom(models.Model):
+class Mom(models.Model):
     name = models.CharField(max_length=120)
     priceM = models.DecimalField(max_digits=4, decimal_places=2)
     priceL = models.DecimalField(max_digits=4, decimal_places=2)
