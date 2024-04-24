ad77165b67eb79a95f9504d2c92724999245adbb
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-24 20:43:10 +0000

    choosemug/models: add class mom and migrate

--- choosemug/models.py
+++ choosemug/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class mom(models.Model):
+    name = models.CharField(max_length=120)
+    priceM = models.DecimalField(max_digits=4, decimal_places=2)
+    priceL = models.DecimalField(max_digits=4, decimal_places=2)
+    pImage = models.URLField()
