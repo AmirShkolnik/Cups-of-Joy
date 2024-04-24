8bbe39e701a283121e0d0c118c5460bb1965d095
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-24 21:22:09 +0000

    choosemug/models - create class Dad and migrate

--- choosemug/models.py
+++ choosemug/models.py
@@ -6,3 +6,9 @@ class Mom(models.Model):
     priceM = models.DecimalField(max_digits=4, decimal_places=2)
     priceL = models.DecimalField(max_digits=4, decimal_places=2)
     pImage = models.URLField()
+
+class Dad(models.Model):
+    name = models.CharField(max_length=120)
+    priceM = models.DecimalField(max_digits=4, decimal_places=2)
+    priceL = models.DecimalField(max_digits=4, decimal_places=2)
+    dImage = models.URLField()
