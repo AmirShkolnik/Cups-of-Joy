9668afb7099d95db70ed3f30cb2a0a0012881cfb
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-24 21:24:56 +0000

    choosemug/admin: add class DadAdmin

--- choosemug/admin.py
+++ choosemug/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Mom
+from .models import Mom, Dad
 # Register your models here.
 
 class MomAdmin(admin.ModelAdmin):
     list_display = ('name', 'priceM', 'priceL')
 
 admin.site.register(Mom, MomAdmin)
+
+class DadAdmin(admin.ModelAdmin):
+    list_display = ('name', 'priceM', 'priceL')
+
+admin.site.register(Dad, DadAdmin)
