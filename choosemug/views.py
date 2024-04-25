d20216a1c7f961aea255f76091f7f55f630cb0fd
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-25 06:37:16 +0000

    choosemug/views: add moms, ctx to mom_view

--- choosemug/views.py
+++ choosemug/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Mom
+# Create your views here.
 
 def mom_view(request):
-    # Add any necessary logic here
-    return render(request, 'choosemug/mom.html')
+    moms = Mom.objects.all()
+    ctx = {'moms': moms}
+    print(moms)
+    return render(request, 'choosemug/mom.html', ctx)
 
 def dad_view(request):
     # Add any necessary logic here
