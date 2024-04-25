d0d3ba7abc2d485e8970440e4c96c7aea60eee95
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-04-25 12:07:17 +0000

    base.html, views.py: add active link to mom, dad and welcome pages

--- choosemug/views.py
+++ choosemug/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Mom
-from .models import Dad
+from .models import Mom, Dad
 # Create your views here.
+
+def welcome_view(request):
+    ctx = {'active_link': 'welcome'}
+    return render(request, 'choosemug/welcome.html', ctx)
 
 def mom_view(request):
     moms = Mom.objects.all()
-    ctx = {'moms': moms}
+    ctx = {'moms': moms, 'active_link': 'mom'}
     print(moms)
     return render(request, 'choosemug/mom.html', ctx)
 
 def dad_view(request):
     dads = Dad.objects.all()
-    ctx = {'dads': dads}
+    ctx = {'dads': dads, 'active_link': 'dad'}
     print(dads)
     return render(request, 'choosemug/dad.html', ctx)
-
-def welcome_view(request):
-    # Add any necessary logic here
-    return render(request, 'choosemug/welcome.html')
