2df9d2895fbab6e03dafb03841a19430d57a5144
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-05 18:28:16 +0000

    urls.py - add path to add.html

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -6,5 +6,6 @@ app_name = 'coffeeshop'
 urlpatterns = [
    path('', views.IndexView.as_view(), name='index'),
    path('add_review/', views.Add_ReviewView.as_view(), name='add_review'),
+   path('add/', views.AddView.as_view(), name='add'),
    path('<slug:slug>/', views.SingleView.as_view(), name='single'),
 ]
