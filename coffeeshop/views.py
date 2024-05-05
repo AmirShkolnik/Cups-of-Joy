afba830f3dbec1eb4baee1929743dbbd68b7031b
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-05 07:19:02 +0000

    coffeeshop/views - add class iNDEXvIEW

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,3 +1,9 @@
+from .models import Core
+from django.views.generic import ListView, DetailView, UpdateView
 from django.shortcuts import render
 
 # Create your views here.
+
+class IndexView(ListView):
+    model= Core
+    template_name = 'coffeeshop/index.html'
