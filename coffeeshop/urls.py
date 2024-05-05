5d51b3c41e2c13102b9cc1f9b40b99207108f332
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-05 18:04:43 +0000

    add add_review, url path add review, views class Add Review and Add

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -5,5 +5,6 @@ app_name = 'coffeeshop'
 
 urlpatterns = [
    path('', views.IndexView.as_view(), name='index'),
+   path('add_review/', views.Add_ReviewView.as_view(), name='add_review'),
    path('<slug:slug>/', views.SingleView.as_view(), name='single'),
 ]
--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,5 +1,6 @@
 from .models import Review
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -12,3 +13,14 @@ class SingleView(DetailView):
     model = Review
     template_name = 'coffeeshop/single.html'
     context_object_name = 'post'
+
+class Add_ReviewView(ListView):
+    model= Review
+    template_name = 'coffeeshop/add_review.html'
+    context_object_name = 'review_list'
+
+class AddView(CreateView):
+    model= Review
+    template_name = 'coffeeshop/add.html'
+    fields = '__all__'
+    success_url = reverse_lazy('coffeeshop:add_review')
