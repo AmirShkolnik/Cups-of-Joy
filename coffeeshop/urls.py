051eff397f3fcb4b6dff0e1a24c9247c21860000
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-06 06:01:23 +0000

    add delete.html, urls - add path delete

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
    path('add_review/', views.Add_ReviewView.as_view(), name='add_review'),
    path('add/', views.AddView.as_view(), name='add'),
    path('<slug:slug>/', views.SingleView.as_view(), name='single'),
-   path('edith/<int:pk>/', views.EditView.as_view(), name='edit'),
+   path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
+   path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
 ]
