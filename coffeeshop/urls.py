d342a86f620abb34630339595421c46c985c84eb
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-29 11:46:42 +0000

    edit.html - change checkbox text to save changes

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
    path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
    path('review/<int:pk>/approve/', views.approve_review, name='approve_review'),
-   path('review/<int:pk>/publish/', views.publish_review, name='publish_review'),
 
 ]
