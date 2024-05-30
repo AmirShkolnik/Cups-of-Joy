9f27ff10fafef29779516ca1cd38a4ba74ca5929
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-30 16:38:25 +0000

    coffeeshop/views - fix lines too long, 51 and 99

---
+++ coffeeshop/views copy.py
@@ -0,0 +1,265 @@
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from .models import Review
+from .forms import PostForm, ReviewForm
+
+# Create your views here.
+
+class IndexView(ListView):
+    model = Review
+    template_name = 'coffeeshop/index.html'
+    context_object_name = 'reviews'
+
+    def get_queryset(self):
+        return Review.objects.filter(status=1, approved=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = self.get_queryset()
+        context['coffeeshops'] = reviews
+        context['no_reviews'] = not reviews.exists()
+        return context
+
+class SingleView(DetailView):
+    """ View a single review """
+    model = Review
+    template_name = 'coffeeshop/single.html'
+    context_object_name = 'post'
+
+class AddReviewView(LoginRequiredMixin, CreateView):
+    template_name = 'coffeeshop/add_review.html'
+    model = Review
+    fields = '__all__'
+    success_url = reverse_lazy('coffeeshop:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Review added successfully.")
+        return super(AddReviewView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_reviews = Review.objects.filter(author=self.request.user).exclude(status=2)  # Exclude deleted reviews
+        context['review_list'] = user_reviews
+        context['no_reviews'] = not user_reviews.exists()
+        return context
+
+class AddView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'coffeeshop/add.html'
+    success_url = reverse_lazy('coffeeshop:add_review')
+
+    def form_valid(self, form):
+        # Save the form instance with the status set by the user
+        form.instance.author = self.request.user
+        self.object = form.save()  # Save the instance to the database
+
+        # Handle the image upload
+        coffee_image = form.cleaned_data.get('coffee_image')
+        if coffee_image:
+            self.object.coffee_image = coffee_image
+            self.object.save()  # Save the instance again after updating the image
+
+        messages.success(self.request, "Review added successfully, waiting for approval.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = messages.get_messages(self.request)
+        return context
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Please fill in all required fields.")
+        return super().form_invalid(form)
+
+class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Review
+    template_name = 'coffeeshop/edit.html'
+    form_class = ReviewForm
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('coffeeshop:single')
+
+    def test_func(self):
+        # Check if the current user is the author of the review or a superuser
+        return self.request.user == self.get_object().author or self.request.user.is_superuser
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, "Review updated and awaiting approval.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Please fill in all required fields.")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        # Redirect to the detail page of the edited content
+        return reverse('coffeeshop:single', kwargs={'slug': self.object.slug})
+    
+class Delete(DeleteView):
+    model = Review
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('coffeeshop:add_review')
+    template_name = 'coffeeshop/confirm-delete.html'
+
+    def form_valid(self, form):
+        # Add a success message
+        messages.success(self.request, "Review deleted successfully.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = messages.get_messages(self.request)
+        return context
+    
+@login_required
+def publish_review(request, pk):
+    review = get_object_or_404(Review, pk=pk, author=request.user)
+    review.status = 1  # Set the status to published
+    review.save()
+    messages.success(request, "Review published successfully.")
+    return redirect('coffeeshop:single', slug=review.slug)
+
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from .models import Review
+from .forms import PostForm, ReviewForm
+
+# Create your views here.
+
+class IndexView(ListView):
+    model = Review
+    template_name = 'coffeeshop/index.html'
+    context_object_name = 'reviews'
+
+    def get_queryset(self):
+        return Review.objects.filter(status=1, approved=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = self.get_queryset()
+        context['coffeeshops'] = reviews
+        context['no_reviews'] = not reviews.exists()
+        return context
+
+class SingleView(DetailView):
+    """ View a single review """
+    model = Review
+    template_name = 'coffeeshop/single.html'
+    context_object_name = 'post'
+
+class AddReviewView(LoginRequiredMixin, CreateView):
+    template_name = 'coffeeshop/add_review.html'
+    model = Review
+    fields = '__all__'
+    success_url = reverse_lazy('coffeeshop:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Review added successfully.")
+        return super(AddReviewView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_reviews = Review.objects.filter(author=self.request.user).exclude(status=2)  # Exclude deleted reviews
+        context['review_list'] = user_reviews
+        context['no_reviews'] = not user_reviews.exists()
+        return context
+
+class AddView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'coffeeshop/add.html'
+    success_url = reverse_lazy('coffeeshop:add_review')
+
+    def form_valid(self, form):
+        # Save the form instance with the status set by the user
+        form.instance.author = self.request.user
+        self.object = form.save()  # Save the instance to the database
+
+        # Handle the image upload
+        coffee_image = form.cleaned_data.get('coffee_image')
+        if coffee_image:
+            self.object.coffee_image = coffee_image
+            self.object.save()  # Save the instance again after updating the image
+
+        messages.success(self.request, "Review added successfully, waiting for approval.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = messages.get_messages(self.request)
+        return context
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Please fill in all required fields.")
+        return super().form_invalid(form)
+
+class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Review
+    template_name = 'coffeeshop/edit.html'
+    form_class = ReviewForm
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('coffeeshop:single')
+
+    def test_func(self):
+        # Check if the current user is the author of the review or a superuser
+        return self.request.user == self.get_object().author or self.request.user.is_superuser
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, "Review updated.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Please fill in all required fields.")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        # Redirect to the detail page of the edited content
+        return reverse('coffeeshop:single', kwargs={'slug': self.object.slug})
+    
+class Delete(DeleteView):
+    model = Review
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('coffeeshop:add_review')
+    template_name = 'coffeeshop/confirm-delete.html'
+
+    def form_valid(self, form):
+        # Add a success message
+        messages.success(self.request, "Review deleted successfully.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = messages.get_messages(self.request)
+        return context
+    
+@login_required
+def publish_review(request, pk):
+    review = get_object_or_404(Review, pk=pk, author=request.user)
+    review.status = 1
+    review.save()
+    messages.success(request, "Review published successfully.")
+    return redirect('coffeeshop:single', slug=review.slug)
+
+@user_passes_test(lambda u: u.is_superuser)
+def approve_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    review.approved = True
+    review.status = 1
+    review.save()
+    messages.success(request, "Review approved and published successfully.")
+    return redirect('coffeeshop:single', slug=review.slug)
+
--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -7,8 +6,11 @@ from django.contrib.auth.decorators import user_passes_test, login_required
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from .models import Review
 from .forms import PostForm, ReviewForm
+from django.views.generic import UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
+
 
 class IndexView(ListView):
     model = Review
@@ -25,11 +27,13 @@ class IndexView(ListView):
         context['no_reviews'] = not reviews.exists()
         return context
 
+
 class SingleView(DetailView):
     """ View a single review """
     model = Review
     template_name = 'coffeeshop/single.html'
     context_object_name = 'post'
+
 
 class AddReviewView(LoginRequiredMixin, CreateView):
     template_name = 'coffeeshop/add_review.html'
@@ -44,10 +48,12 @@ class AddReviewView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user_reviews = Review.objects.filter(author=self.request.user).exclude(status=2)  # Exclude deleted reviews
+        user_reviews = Review.objects.filter(
+            author=self.request.user).exclude(status=2)
         context['review_list'] = user_reviews
         context['no_reviews'] = not user_reviews.exists()
         return context
+
 
 class AddView(LoginRequiredMixin, CreateView):
     model = Review
@@ -61,12 +67,14 @@ class AddView(LoginRequiredMixin, CreateView):
         self.object = form.save()  # Save the instance to the database
 
         # Handle the image upload
+        # Save the instance again after updating the image
         coffee_image = form.cleaned_data.get('coffee_image')
         if coffee_image:
             self.object.coffee_image = coffee_image
-            self.object.save()  # Save the instance again after updating the image
+            self.object.save()
 
-        messages.success(self.request, "Review added successfully, waiting for approval.")
+        messages.success(self.request, "Review added successfully, "
+                         "waiting for approval.")
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
@@ -77,6 +85,7 @@ class AddView(LoginRequiredMixin, CreateView):
     def form_invalid(self, form):
         messages.error(self.request, "Please fill in all required fields.")
         return super().form_invalid(form)
+
 
 class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Review
@@ -87,135 +96,8 @@ class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         # Check if the current user is the author of the review or a superuser
-        return self.request.user == self.get_object().author or self.request.user.is_superuser
-
-    def form_valid(self, form):
-        self.object = form.save()
-        messages.success(self.request, "Review updated and awaiting approval.")
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "Please fill in all required fields.")
-        return super().form_invalid(form)
-
-    def get_success_url(self):
-        # Redirect to the detail page of the edited content
-        return reverse('coffeeshop:single', kwargs={'slug': self.object.slug})
-    
-class Delete(DeleteView):
-    model = Review
-    pk_url_kwarg = 'pk'
-    success_url = reverse_lazy('coffeeshop:add_review')
-    template_name = 'coffeeshop/confirm-delete.html'
-
-    def form_valid(self, form):
-        # Add a success message
-        messages.success(self.request, "Review deleted successfully.")
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['messages'] = messages.get_messages(self.request)
-        return context
-    
-@login_required
-def publish_review(request, pk):
-    review = get_object_or_404(Review, pk=pk, author=request.user)
-    review.status = 1  # Set the status to published
-    review.save()
-    messages.success(request, "Review published successfully.")
-    return redirect('coffeeshop:single', slug=review.slug)
-
-from django.contrib import messages
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.shortcuts import render, redirect, get_object_or_404, reverse
-from .models import Review
-from .forms import PostForm, ReviewForm
-
-# Create your views here.
-
-class IndexView(ListView):
-    model = Review
-    template_name = 'coffeeshop/index.html'
-    context_object_name = 'reviews'
-
-    def get_queryset(self):
-        return Review.objects.filter(status=1, approved=True)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = self.get_queryset()
-        context['coffeeshops'] = reviews
-        context['no_reviews'] = not reviews.exists()
-        return context
-
-class SingleView(DetailView):
-    """ View a single review """
-    model = Review
-    template_name = 'coffeeshop/single.html'
-    context_object_name = 'post'
-
-class AddReviewView(LoginRequiredMixin, CreateView):
-    template_name = 'coffeeshop/add_review.html'
-    model = Review
-    fields = '__all__'
-    success_url = reverse_lazy('coffeeshop:index')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        messages.success(self.request, "Review added successfully.")
-        return super(AddReviewView, self).form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_reviews = Review.objects.filter(author=self.request.user).exclude(status=2)  # Exclude deleted reviews
-        context['review_list'] = user_reviews
-        context['no_reviews'] = not user_reviews.exists()
-        return context
-
-class AddView(LoginRequiredMixin, CreateView):
-    model = Review
-    form_class = ReviewForm
-    template_name = 'coffeeshop/add.html'
-    success_url = reverse_lazy('coffeeshop:add_review')
-
-    def form_valid(self, form):
-        # Save the form instance with the status set by the user
-        form.instance.author = self.request.user
-        self.object = form.save()  # Save the instance to the database
-
-        # Handle the image upload
-        coffee_image = form.cleaned_data.get('coffee_image')
-        if coffee_image:
-            self.object.coffee_image = coffee_image
-            self.object.save()  # Save the instance again after updating the image
-
-        messages.success(self.request, "Review added successfully, waiting for approval.")
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['messages'] = messages.get_messages(self.request)
-        return context
-
-    def form_invalid(self, form):
-        messages.error(self.request, "Please fill in all required fields.")
-        return super().form_invalid(form)
-
-class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Review
-    template_name = 'coffeeshop/edit.html'
-    form_class = ReviewForm
-    pk_url_kwarg = 'pk'
-    success_url = reverse_lazy('coffeeshop:single')
-
-    def test_func(self):
-        # Check if the current user is the author of the review or a superuser
-        return self.request.user == self.get_object().author or self.request.user.is_superuser
+        return self.request.user == self.get_object(
+            ).author or self.request.user.is_superuser
 
     def form_valid(self, form):
         self.object = form.save()
@@ -229,7 +111,8 @@ class EditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_success_url(self):
         # Redirect to the detail page of the edited content
         return reverse('coffeeshop:single', kwargs={'slug': self.object.slug})
-    
+
+
 class Delete(DeleteView):
     model = Review
     pk_url_kwarg = 'pk'
@@ -245,7 +128,8 @@ class Delete(DeleteView):
         context = super().get_context_data(**kwargs)
         context['messages'] = messages.get_messages(self.request)
         return context
-    
+
+
 @login_required
 def publish_review(request, pk):
     review = get_object_or_404(Review, pk=pk, author=request.user)
@@ -253,6 +137,7 @@ def publish_review(request, pk):
     review.save()
     messages.success(request, "Review published successfully.")
     return redirect('coffeeshop:single', slug=review.slug)
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def approve_review(request, pk):
@@ -262,4 +147,3 @@ def approve_review(request, pk):
     review.save()
     messages.success(request, "Review approved and published successfully.")
     return redirect('coffeeshop:single', slug=review.slug)
-
