79c89f35f72278f0d4a47b8915dc6e621b407250
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-30 16:07:49 +0000

    coffeeshop/models - add white spaces

--- coffeeshop/models.py
+++ coffeeshop/models.py
@@ -8,11 +8,14 @@ from django_resized import ResizedImageField
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Review(models.Model):
 
-    title = models.CharField(max_length=200, null=False, blank=False, default='Coffee Shop Name')
+    title = models.CharField(max_length=200, null=False,
+                             blank=False, default='Coffee Shop Name')
     slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
+    author = models.ForeignKey(User, on_delete=models.
+                               CASCADE, related_name='review')
     content = models.TextField(null=False, blank=False)
     excerpt = models.TextField(max_length=300, null=False, blank=False)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -20,13 +23,17 @@ class Review(models.Model):
     published = models.DateTimeField(default=timezone.now)
     approved = models.BooleanField(default=False)
     coffee_image = CloudinaryField('image', null=True, blank=True)
-    image_alt = models.CharField(max_length=100, null=False, blank=False, default='Coffee Shop Image')
+    image_alt = models.CharField(max_length=100, null=False,
+                                 blank=False, default='Coffee Shop Image')
+
 
 def get_absolute_url(self):
     return reverse('coffeeshop:single', args=[self.slug])
 
+
 class Meta:
     ordering = ['-published']
 
+
 def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+    return f"Comment {self.body} by {self.author}"
