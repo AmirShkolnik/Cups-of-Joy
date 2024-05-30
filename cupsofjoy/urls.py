144cb49c21e8b3c0077876c0418d33b19d3102c3
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-05-30 14:19:24 +0000

    urls.py - fix line too long

--- cupsofjoy/urls.py
+++ cupsofjoy/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('coffeeshop/', include('coffeeshop.urls', namespace='coffeeshop')),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
