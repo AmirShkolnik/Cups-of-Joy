4fbdf48dd70ebfaadbcf98088ab5841af7c5e3d3
ASHKO <amirshkolnik1@gmail.com>
2026-01-03 19:21:32 +0100

    Add db-check route to root urls

--- cupsofjoy/urls.py
+++ cupsofjoy/urls.py
@@ -1,34 +1,30 @@
 """
-URL configuration for my_project project.
+URL configuration for cupsofjoy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+https://docs.djangoproject.com/en/4.2/topics/http/urls/
 """
+
 from django.contrib import admin
-from django.conf.urls import handler403, handler404, handler500
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from cupsofjoy import views
-from .views import custom_404, custom_500
+
+from .views import custom_403, custom_404, custom_500, db_check
+
 
 # Custom error handlers
 handler403 = 'cupsofjoy.views.custom_403'
 handler404 = 'cupsofjoy.views.custom_404'
 handler500 = 'cupsofjoy.views.custom_500'
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # TEMP: DB debug endpoint (remove after verifying)
+    path('db-check/', db_check),
+
     path('about/', include('about.urls'), name='about-urls'),
     path('accounts/', include('allauth.urls')),
     path('coffeeshop/', include('coffeeshop.urls', namespace='coffeeshop')),
@@ -36,6 +32,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
